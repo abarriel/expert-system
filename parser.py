2628484609dd5b60ebe9e478a0d8acc3047c2db7
abarriel <abarriel@student.42.fr>
2018-01-31 00:43:36 +0100

    start dic

--- parser.py
+++ parser.py
@@ -2,8 +2,20 @@
 import sys
 import re
 
+def dict_gen(k):
+    yield chr(k+65)
+    yield { 'value': None }
+
+class bc:
+    BLUE = '\033[1;96m'
+    GREEN = '\033[1;92m'
+    YELLOW = '\033[1;93m'
+    RED = '\033[1;91m'
+    RES = '\033[0m'
+    BOLD = '\033[1m'
+
 if len(sys.argv) != 2:
-    print("Add a file in parameters")
+    print(bc.RED, "Add a file in parameters", bc.RES)
     sys.exit(1)
 
 reSpaceandCom = ["[\r\t\f\v ]|\#.*", ""]
@@ -15,12 +27,19 @@ listToDict = lambda array, value: dict((v, value) for v in array)
 filterFirstChar = lambda array, char: list([x for x in array if x[0] == char][0])
 
 getGoals = lambda file: listToDict((filterFirstChar(file, '?')[1:]), False)
-getRules = lambda file: listToDict((filterFirstChar(file, '=')[1:]), True)
+getFacts = lambda file: listToDict((filterFirstChar(file, '=')[1:]), True)
 
 fileBrut = open(sys.argv[1], "r").read()
 file = cleanFileAndReturnList(fileBrut)
 goals = getGoals(file)
-rules =  getRules(file)
+facts = getFacts(file)
+rules = file[:-2]
+dic = dict(map(dict_gen, range(26)))
 
-print(goals)
-print(rules)
+if not goals:
+    print(bc.RED, "no goals providen", bc.RES)
+    sys.exit(1)
+
+print(bc.GREEN + 'Facts:', facts, bc.RES, "\n")
+print(bc.BLUE + 'Goals:', goals, bc.RES, "\n")
+print(bc.YELLOW + 'Rules:\n\n' + "\n".join(rules), bc.RES, "\n")
