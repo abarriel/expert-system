6c36e84d4d163b329e99914fbf02780a1f3f9282
abarriel <abarriel@student.42.fr>
2018-01-30 18:08:13 +0100

    file clean

--- parser.py
+++ parser.py
@@ -1,19 +1,26 @@
-if __name__ == '__main__':
-    year = input('Write a year: ')
-    print(year)
-    year = int(year)
-    bisex = False
+#!/usr/bin/python3
+import sys
+import re
 
-    if year % 400 == 0:
-        bisex = True
-    elif year % 100 == 0:
-        bisex = False
-    elif year % 4 == 0:
-        bisex = True
-    else:
-        bisex = False
+if len(sys.argv) != 2:
+    print("Add a file in parameters")
+    sys.exit(1)
 
-    if bisex:
-        print("Year ibisex")
-    else:
-        print("Year is not a bisex")
+reSpaceandCom = ["[\r\t\f\v ]|\#.*", ""]
+reAvoidDuplicate = ["\n+", "\n"]
+
+cleanFileAndReturnList = lambda file: re.sub(*(reAvoidDuplicate + [re.sub(*(reSpaceandCom + [file]))])).strip().split('\n')
+filterByFirst = lambda file, char: list(filter(file, file[0] == char))
+listToDict = lambda array, value: dict((v, value) for v in array)
+filterFirstChar = lambda array, char: list([x for x in array if x[0] == char][0])
+
+getGoals = lambda file: listToDict((filterFirstChar(file, '?')[1:]), False)
+getRules = lambda file: listToDict((filterFirstChar(file, '=')[1:]), True)
+
+fileBrut = open(sys.argv[1], "r").read()
+file = cleanFileAndReturnList(fileBrut)
+goals = getGoals(file)
+rules =  getRules(file)
+
+print(goals)
+print(rules)
